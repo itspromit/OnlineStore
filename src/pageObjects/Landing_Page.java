45e064116aa812be9f277fd11c27e86c7e697e6e
Mukesh <Mukesh@DESKTOP-7E7STVE>
2021-04-02 17:01:09 +0530

    changes

--- src/pageObjects/Landing_Page.java
+++ src/pageObjects/Landing_Page.java
@@ -13,60 +13,67 @@ import utility.Utils;
     public class Landing_Page {
            
     	
-        WebDriver driver;   
+        //WebDriver driver;   
         
         @FindBy(xpath="//a[text()='Desktops']")
-        WebElement Desktops_menu;
+        public WebElement Desktops_menu;
         
         @FindBy(xpath="//a[text()='Laptops & Notebooks']")
-        WebElement LaptopsandNotebooks_menu;
+        public WebElement LaptopsandNotebooks_menu;
         
         @FindBy(xpath="//a[text()='Components']")
-        WebElement Components_menu;
+        public WebElement Components_menu;
         
         @FindBy(xpath="//a[text()='Tablets']")
-        WebElement Tablets_menu;
+        public WebElement Tablets_menu;
        
         
         @FindBy(xpath="//a[text()='Software']")
-        WebElement Software_menu;
+        public WebElement Software_menu;
         
         @FindBy(xpath="//a[text()='Phones & PDAs']")
-        WebElement PhonesandPDAs_menu;
+        public WebElement PhonesandPDAs_menu;
         
         @FindBy(xpath="//a[text()='Cameras']")
-        WebElement Cameras_menu;
+        public WebElement Cameras_menu;
         
         
         @FindBy(xpath="//a[text()='MP3 Players']")
-        WebElement MP3Players_menu;
+        public WebElement MP3Players_menu;
         
         @FindBy(xpath="//a[text()='Your Store']")
-        WebElement YourStore;
+        public WebElement YourStore;
         
         @FindBy(xpath="//span[text()='123456789']")
-        WebElement Contact_No;
+        public WebElement Contact_No;
         
         @FindBy(xpath="//a[@class='fa fa-phone']")
-        WebElement Contact_Us;
+        public WebElement Contact_Us;
         
         @FindBy(xpath="//a[@class='dropdown-toggle']")
-        WebElement MyAccount_menu;
-        
-        @FindBy(xpath="//span[text()='My Account']")
-        WebElement MyAccount_title;
+        public WebElement MyAccount_menu;
         
         @FindBy(xpath="//ul[@class='dropdown-menu dropdown-menu-right']")
-        WebElement MyAccount_dropdown;
+        public WebElement MyAccount_dropdown;
         
         @FindBy(id="wishlist-total")
-        WebElement WishList_menu;
+        public WebElement WishList_menu;
         
         @FindBy(xpath="//a[text()='Shopping Cart']")
-        WebElement ShoppingCart_menu;
+        public WebElement ShoppingCart_menu;
         
         @FindBy(xpath="//a[text()='Checkout']")
-        WebElement Checkout_menu;
+        public WebElement Checkout_menu;
+        
+        @FindBy(xpath="//input[@class='form-control input-lg']")
+        public WebElement Search_txtbox;
+        
+        
+        @FindBy(xpath="//button[@class='form-control input-lg']")
+        public WebElement Search_button;
+        
+        @FindBy(id="cart")
+        public WebElement Cart;
         
       
     }
