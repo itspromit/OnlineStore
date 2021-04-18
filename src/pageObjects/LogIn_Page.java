d0d67afda5544862e358c15a7146d10e8176e2b7
Mukesh <Mukesh@DESKTOP-7E7STVE>
2021-04-18 13:59:41 +0530

    changes

--- src/pageObjects/LogIn_Page.java
+++ src/pageObjects/LogIn_Page.java
@@ -1,43 +1,59 @@
 package pageObjects;
-        import org.openqa.selenium.*;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindAll;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 import utility.Log;
-    public class LogIn_Page extends BaseClass {
-           private static WebElement element = null;
-        
-        public LogIn_Page(WebDriver driver){
-            	super(driver);
-        }     
-        public static WebElement txtbx_UserName() throws Exception{
-        	try{
-	            element = driver.findElement(By.id("log"));
-	            Log.info("Username text box is found on the Login Page");
-        	}catch (Exception e){
-           		Log.error("UserName text box is not found on the Login Page");
-           		throw(e);
-           		}
-           	return element;
-            }
-        
-        public static WebElement txtbx_Password() throws Exception{
-        	try{
-	        	element = driver.findElement(By.id("pwd"));
-	            Log.info("Password text box is found on the Login page");
-        	}catch (Exception e){
-        		Log.error("Password text box is not found on the Login Page");
-           		throw(e);
-           		}
-           	return element;
-        }
-        
-        public static WebElement btn_LogIn() throws Exception{
-        	try{
-	        	element = driver.findElement(By.id("login"));
-	            Log.info("Submit button is found on the Login page");
-        	}catch (Exception e){
-        		Log.error("Submit button is not found on the Login Page");
-           		throw(e);
-           		}
-           	return element;
-        }
+import utility.Utils;
+    public class LogIn_Page  {
+    	@FindBy(xpath="//a[text()='Login']")
+        public WebElement Login_menu;
+    	
+    	@FindBy(xpath="//h2[text()='New Customer']")
+        public WebElement NewCustomer_header;
+    	
+    	@FindBy(xpath="//h2[text()='New Customer']/ancestor::div[@class='col-sm-6']")
+        public WebElement NewCustomer_menu;
+    	
+    	@FindBy(xpath="//h2[text()='New Customer']/ancestor::div[@class='col-sm-6']/descendant::strong[text()='Register Account']")
+    	public WebElement RegisterAccount_label;
+    	
+    	@FindBy(xpath="//h2[text()='New Customer']/ancestor::div[@class='col-sm-6']/descendant::p[2]")
+        public WebElement NewCustomer_text;
+    	
+    	@FindBy(xpath="//h2[text()='New Customer']/ancestor::div[@class='col-sm-6']/descendant::a[text()='Continue']")
+        public WebElement Continue_button;
+    	
+    	@FindBy(xpath="//h2[text()='Returning Customer']")
+        public WebElement ReturningCustomer_header;
+    	
+    	@FindBy(xpath="//h2[text()='Returning Customer']/ancestor::div[@class='col-sm-6']")
+    	public WebElement ReturningCustomer_menu;
+    	
+    	@FindBy(xpath="//h2[text()='Returning Customer']/ancestor::div[@class='col-sm-6']/descendant::strong[text()='I am a returning customer']")
+    	public WebElement ReturningCustomer_text;
+    	
+    	@FindBy(xpath="//h2[text()='Returning Customer']/ancestor::div[@class='col-sm-6']/descendant::label[text()='E-Mail Address']")
+    	public WebElement EmailAddress_label;
+    	
+    	@FindBy(xpath="//h2[text()='Returning Customer']/ancestor::div[@class='col-sm-6']/descendant::label[@id='input-email']")
+    	public WebElement EmailAddress_field;
+    	
+    	@FindBy(xpath="//h2[text()='Returning Customer']/ancestor::div[@class='col-sm-6']/descendant::label[text()='Password']")
+    	public WebElement Password_label;
+    	
+    	@FindBy(xpath="//h2[text()='Returning Customer']/ancestor::div[@class='col-sm-6']/descendant::label[@id='input-password']")
+    	public WebElement Password_field;
+    	
+    	@FindBy(xpath="//h2[text()='Returning Customer']/ancestor::div[@class='col-sm-6']/descendant::a[text()='Forgotten Password']")
+    	public WebElement ForgotPassword_link;
+    	
+    	@FindBy(xpath="//h2[text()='Returning Customer']/ancestor::div[@class='col-sm-6']/descendant::input[@class='btn btn-primary']")
+    	public WebElement Login_button;
+    	
     }
