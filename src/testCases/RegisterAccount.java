ecef913da1cbd533d48e310901d790dc5b2d24f1
Mukesh <Mukesh@DESKTOP-7E7STVE>
2021-05-04 22:12:22 +0530

    changes made

---
+++ src/testCases/RegisterAccount.java
@@ -0,0 +1,57 @@
+package testCases;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import pageObjects.Landing_Page;
+import pageObjects.RegisterAccount_Page;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeGroups;
+import org.testng.annotations.AfterGroups;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+import utility.CommonFunctionandEvents;
+import utility.ExtentManager;
+import utility.Utils;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+import utility.Log;
+import utility.Constant;
+
+public class RegisterAccount {
+
+	WebDriver driver;
+	RegisterAccount_Page R_Acc;
+	
+	@BeforeClass
+	public void fnCheck() {
+		try {
+			R_Acc = new RegisterAccount_Page(driver);
+			if (driver.getTitle().contains("Register Account")) {
+				Log.info("User on landing page");
+				System.out.println("User on Register Account page");
+			} else {
+				System.out.println("User not on RegisterAccount page");
+				driver.get("http://tutorialsninja.com/demo/index.php?route=account/register");
+
+			}
+		} catch (Exception e) {
+			String Ex = e.toString();
+			System.out.println(Ex);
+		}
+
+	}
+
+}
